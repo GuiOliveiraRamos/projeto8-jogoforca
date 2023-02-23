3af5c3389dc25c77a61c560e24cd41eac1b23e52
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-02-23 16:27:41 -0300

    tentativas de implementar verificação de letras

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./css/reset.css"
 import "./css/style.css"
 import Jogo from "./jogo";
-import Letras from "./letras";
 
 
 function App() {
   return (
     <div className="App">
       <Jogo />
-      <Letras />
-    </div>
+        </div>
   );
 }
 
--- src/jogo.js
+++ src/jogo.js
@@ -1,46 +1,84 @@
-import palavras from "./palavras";
-import forca0 from "./assets/forca0.png";
-import forca1 from "./assets/forca1.png";
-import forca2 from "./assets/forca2.png";
-import forca3 from "./assets/forca3.png";
-import forca4 from "./assets/forca4.png";
-import forca5 from "./assets/forca5.png";
-import forca6 from "./assets/forca6.png";
-import { useState } from "react";
+import palavras from "./palavras"
+import forca0 from "./assets/forca0.png"
+import forca1 from "./assets/forca1.png"
+import forca2 from "./assets/forca2.png"
+import forca3 from "./assets/forca3.png"
+import forca4 from "./assets/forca4.png"
+import forca5 from "./assets/forca5.png"
+import forca6 from "./assets/forca6.png"
+import { useState } from "react"
+import Letras from "./Letras"
 
-export default function Jogo() {
+export default function Jogo({}) {
 
-  const [escolherPalavra, setEscolherPalavra] = useState("");
-  const [palavraEscolhida, setPalavraEscolhida] = useState([]);
+  const [escolherPalavra, setEscolherPalavra] = useState("")
+  const [palavraEscolhida, setPalavraEscolhida] = useState([])
+  const [erros, setErros] = useState(0)
+  const [ganhou, setGanhou] = useState(false)
 
-  const images = [forca0, forca1, forca2, forca3, forca4, forca5, forca6];
+  const images = [forca0, forca1, forca2, forca3, forca4, forca5, forca6]
 
   const palavraAleatoria = () => {
-    const aleatoria = Math.floor(Math.random() * palavras.length);
-    return palavras[aleatoria];
+
+    const aleatoria = Math.floor(Math.random() * palavras.length)
+
+    return palavras[aleatoria]
   };
 
   const comecarJogo = () => {
-    const pegarAleatoria = palavraAleatoria();
-    setEscolherPalavra(pegarAleatoria);
+
+    const pegarAleatoria = palavraAleatoria()
+
+    setEscolherPalavra(pegarAleatoria)
+    
     setPalavraEscolhida(
       pegarAleatoria.split("").map((letra) => ({ valor: letra, acertou: false }))
-    );
-  };
+    )
 
+    setErros(0)
+
+    setGanhou(false)
+
+  }
+    
+  const underline = (letra) => {
+    return <span className = "editar-underline">{letra}</span>
+  }
+
+  const verificarLetra = (letra) => {
+    let acertou = false;
+    setPalavraEscolhida(
+      palavraEscolhida.map((l) => {
+        if (l.valor === letra) {
+          acertou = true;
+          return { ...l, acertou: true };
+        } else {
+          return l;
+        }
+      })
+    );
+    if (!acertou) {
+      setErros(erros + 1);
+    }
+  };
+ 
+  
   return (
     <div className="container-pai">
       <div key="container" className="container">
-        <img className="" src={images[0]} alt={images[0]}></img>
-        <div>
-        <button onClick={comecarJogo}>Escolher Palavra</button>
-           <div>
-        {palavraEscolhida.map((letra, index) => (
-          <span key={index}>{letra.acertou ? letra.valor : "_"}</span>
-        ))}
-           </div>
+        <img className="" src={images[erros]} alt={images[erros]}></img>
+        <div className="container-right">
+          <button onClick={comecarJogo}>Escolher Palavra</button>
+          <div >
+            {palavraEscolhida.map((letra, index) => (
+              <span key={index}>
+                {letra.acertou ? letra.valor : underline("_")}
+              </span>
+            ))}
+          </div>
+        </div>
       </div>
-      </div>
+      <Letras verificarLetra={verificarLetra} />
     </div>
-  );
+  )
 }
