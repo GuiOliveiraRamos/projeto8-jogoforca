ed6d8e6016c65be9a3bd675891132da23ef304f1
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-02-27 10:50:16 -0300

    praticamente pronto

--- src/Letras.js
+++ src/Letras.js
@@ -1,11 +1,11 @@
-export default function Letras({ verificarLetra }) {
+export default function Letras({ verificarLetra, ativarLetras}) {
     
     const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   
     return (
       <div className="alfabeto">
         {alfabeto.map((letra) => (
-          <button data-test="letter"key={letra} onClick={() => verificarLetra(letra)}>
+          <button data-test="letter" disabled={ativarLetras} key={letra} onClick={() => verificarLetra(letra)}>
             {letra}
           </button>
         ))}
--- src/jogo.js
+++ src/jogo.js
@@ -6,7 +6,7 @@ import forca3 from "./assets/forca3.png"
 import forca4 from "./assets/forca4.png"
 import forca5 from "./assets/forca5.png"
 import forca6 from "./assets/forca6.png"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Letras from "./Letras"
 
 export default function Jogo({}) {
@@ -14,7 +14,10 @@ export default function Jogo({}) {
   const [escolherPalavra, setEscolherPalavra] = useState("")
   const [palavraEscolhida, setPalavraEscolhida] = useState([])
   const [erros, setErros] = useState(0)
-
+  const [ativarLetras, setAtivarLetras] = useState(true)
+  const [ganhou, setGanhou] = useState(false)
+  const [perdeu, setPerdeu] = useState(false)
+  
   const images = [forca0, forca1, forca2, forca3, forca4, forca5, forca6]
 
   const palavraAleatoria = () => {
@@ -35,12 +38,35 @@ export default function Jogo({}) {
     )
 
     setErros(0)
+    setAtivarLetras(false)
+    setGanhou(false)
+    setPerdeu(false)
 
   }
     
   const underline = () => {
     return <span className="editar-underline">_</span>
   }
+
+  useEffect(() => {
+
+    if (!palavraEscolhida.some((letra) => !letra.acertou)) {
+
+      setGanhou(true)
+
+    }
+  }, [palavraEscolhida, erros])
+
+
+  useEffect(() => {
+
+    if (erros + 1 === images.length) {
+
+      setPerdeu(true)
+      setAtivarLetras(true)
+
+    }
+  }, [erros])
 
   const verificarLetra = (letra) => {
 
@@ -69,6 +95,7 @@ export default function Jogo({}) {
       setErros(erros + 1)
 
     }
+
   }
   
   return (
@@ -77,16 +104,16 @@ export default function Jogo({}) {
         <img data-test="game-image" className="" src={images[erros]} alt={images[erros]}></img>
         <div className="container-right">
           <button data-test = "choose-word" onClick={comecarJogo}>Escolher Palavra</button>
-          <div >
+          <div className={`palavra-escolhida ${ganhou ? 'acertou' : ''} ${perdeu ? 'errou' : ''}`}>
             {palavraEscolhida.map((letra, index) => (
               <span data-test="word" key={index}>
-                {letra.acertou ? letra.valor : underline("_")}
+                {letra.acertou || perdeu ? letra.valor : underline("_")}
               </span>
             ))}
           </div>
         </div>
       </div>
-      <Letras verificarLetra={verificarLetra} />
+      <Letras ativarLetras={ativarLetras} verificarLetra={verificarLetra} />
     </div>
   )
 }
